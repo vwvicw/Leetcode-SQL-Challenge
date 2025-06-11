c1853449016e4375e10ce3540cbe1ee3100761c2
vwvicw <92187581+vwvicw@users.noreply.github.com>
2025-06-11 16:45:42 -0400

    Create 1789. Primary Department for Each Employee.sql

---
+++ Easy/1789. Primary Department for Each Employee.sql
@@ -0,0 +1,23 @@
+
+SELECT e1.employee_id, e1.department_id
+FROM Employee e1
+LEFT JOIN (SELECT employee_id, department_id, 1 AS only_dep
+    FROM Employee 
+    GROUP BY employee_id
+    HAVING COUNT(*)=1) AS e2
+ON e1.employee_id = e2.employee_id AND e1.department_id = e2.department_id
+WHERE primary_flag = 'Y' OR only_dep = 1
+
+/* Alternative Solution: UNION
+
+SELECT employee_id, department_id FROM Employee
+WHERE primary_flag = 'Y'
+
+UNION
+
+SELECT employee_id, department_id
+FroM Employee
+GROUP BY 1
+HAVING 
+
+
